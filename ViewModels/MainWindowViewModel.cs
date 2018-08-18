4dd23e5e7632ff15444f2a9dbb51f414900dd351
huntergaston <41171965+huntergaston@users.noreply.github.com>
2018-08-18 12:57:26 -0500

    initial commit

---
+++ ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reactive;
+using System.Text;
+using ReactiveUI;
+
+namespace AvaloniaTest.ViewModels
+{
+    public class MainWindowViewModel : ViewModelBase
+    {
+        private int _number;
+
+        public int Number
+        {
+            get => _number;
+            set => this.RaiseAndSetIfChanged(ref _number, value);
+        }
+    
+
+        public MainWindowViewModel()
+        {
+        
+            Increment = ReactiveCommand.Create(IncrementNumber);
+
+        }
+        public ReactiveCommand<Unit, Unit> Increment { get; }
+
+        void IncrementNumber()
+        {
+            Number += 1;
+
+
+        }
+    }
+}
